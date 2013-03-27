e04d5afbe8213ee075244a4e308173305ab18e84
Tijn <tijn@divbyzero.nl>
2013-03-27 17:46:18 +0100

    Gefixte world en shizzle

--- opgave4.pl
+++ opgave4.pl
@@ -1,9 +1,7 @@
 /*
- *
  *  INFOIIS ASSIGNMENT 4
  *  Tijn Kersjes (3855473)
  *  Jordi Vermeulen (3835634)
- *
  */
 
 :- [world].
@@ -15,6 +13,30 @@ safe(X, Y) :- \+ possibleDanger(X, Y).
 possibleWumpus(X, Y) :- stench(U, V), aangrenzend(X, Y, U, V).
 possiblePit(X, Y) :- breeze(U, V), aangrenzend(X, Y, U, V).
 aangrenzend(X, Y, U, V) :- links(X, Y, U, V); rechts(X, Y, U, V); boven(X, Y, U, V); onder(X, Y, U, V).
+
+seenSurrounded(X, Y, Z) :-  seenDirection(X, Y, Z, boven), 
+							seenDirection(X, Y, Z, onder),
+							seenDirection(X, Y, Z, links),
+							seenDirection(X, Y, Z, rechts).
+
+% 'Delegates'  --->  call(Z, X, Y)  ---->   Z(X, Y)
+seenDirection(X, Y, _, D) :- \+ call(D, X, Y, _, _).
+seenDirection(X, Y, Z, D) :- call(D, X, Y, X2, Y2), call(Z, X2, Y2). % and check if agent has seen X2,Y2 !
+
+wumpus(X, Y) :- seenSurrounded(X, Y, stench).
+pit(X, Y) :- seenSurrounded(X, Y, breeze).
+
+/*
+wumpus(X, Y) :- stench(X1, Y1), boven(X, Y, X1, Y1), 
+				stench(X2, Y2), onder(X, Y, X2, Y2), 
+				stench(X3, Y3), links(X, Y, X3, Y3), 
+				stench(X4, Y4), rechts(X, Y, X4, Y4).
+
+pit(X, Y) :- 	breeze(X1, Y1), boven(X, Y, X1, Y1), 
+				breeze(X2, Y2), onder(X, Y, X2, Y2), 
+				breeze(X3, Y3), links(X, Y, X3, Y3), 
+				breeze(X4, Y4), rechts(X, Y, X4, Y4).
+*/
 
 % run/1
 
--- world.pl
+++ world.pl
@@ -19,8 +19,8 @@
  */
 
 % pits
-breeze(X, Y) :- aangrenzend(X, Y, 3, 1).  
-breeze(X, Y) :- aangrenzend(X, Y, 3, 3). 
+breeze(X, Y) :- aangrenzend(X, Y, 3, 1).
+breeze(X, Y) :- aangrenzend(X, Y, 3, 3).
 breeze(X, Y) :- aangrenzend(X, Y, 4, 3).
 
 % wumpus
@@ -31,23 +31,23 @@ glitter(2, 3).
 
 /*
  *  INTERFACE:
- *  kamer(?X,?Y) - geeft aan of (X,Y) een kamer is.
- *  rechts(?X,?Y,?U,?V) - geeft aan dat (X,Y) rechts van (U,V) ligt.
- *  links(?X,?Y,?U,?V) - geeft aan dat kamer (X,Y) links van kamer (U,V) ligt.
- *  boven(?X,?Y,?U,?V) geeft aan dat kamer (X,Y) boven kamer (U,V) ligt.
- *  onder(?X,?Y,?U,?V) - geeft aan dat kamer (X,Y) onder kamer (U,V) ligt.
- *  glitter(+X,+Y) - slaagt als er op (X,Y) een glitter is waar te nemen.￼
- *  breeze(+X,+Y) - slaagt als er op (X,Y) een breeze waar te nemen is.
- *  stench(+X,+Y) - slaagt als er op (X,Y) een stench waar te nemen is.
+ *  kamer(?X,?Y) - geeft aan of (X,Y) een kamer is
+ *  rechts(?X,?Y,?U,?V) - geeft aan dat (X,Y) rechts van (U,V) ligt
+ *  links(?X,?Y,?U,?V) - geeft aan dat kamer (X,Y) links van kamer (U,V) ligt
+ *  boven(?X,?Y,?U,?V) geeft aan dat kamer (X,Y) boven kamer (U,V) ligt
+ *  onder(?X,?Y,?U,?V) - geeft aan dat kamer (X,Y) onder kamer (U,V) ligt
+ *  glitter(+X,+Y) - slaagt als er op (X,Y) een glitter is waar te nemen
+ *  breeze(+X,+Y) - slaagt als er op (X,Y) een breeze waar te nemen is
+ *  stench(+X,+Y) - slaagt als er op (X,Y) een stench waar te nemen is
  */
 
 tussen(X,Y,X) :- X =< Y.
 tussen(X,Y,Z) :- A is X + 1, A =< Y, tussen(A,Y,Z).
 
 kamer(X, Y) :- tussen(1, 4, X), tussen(1, 4, Y).
-rechts(X, Y, U, V) :- kamer(X, Y), kamer(U, V), Y = V, U is X + 1.
-links(X, Y, U, V) :- kamer(X, Y), kamer(U, V), Y = V, U is X - 1.
-boven(X, Y, U, V) :- kamer(X, Y), kamer(U, V), Y = V - 1, X = U.
-onder(X, Y, U, V) :- kamer(X, Y), kamer(U, V), Y = V + 1, X = U.
+rechts(X, Y, U, V) :- kamer(X, Y), kamer(U, V), Y is V, U is X + 1.
+links(X, Y, U, V) :- kamer(X, Y), kamer(U, V), Y is V, U is X - 1.
+boven(X, Y, U, V) :- kamer(X, Y), kamer(U, V), Y is V - 1, X = U.
+onder(X, Y, U, V) :- kamer(X, Y), kamer(U, V), Y is V + 1, X = U.
 
 aangrenzend(X, Y, U, V) :- links(X, Y, U, V); rechts(X, Y, U, V); boven(X, Y, U, V); onder(X, Y, U, V).
